35f372a75744278737cc2f74aded73782e417600
RKintanar <Ryan.Kintanar@gmail.com>
2016-10-16 13:16:57 +0800

    Initial Commit
    Rotating Red Cube with Ads Service enabled

---
+++ Assets/UnityAdsButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class UnityAdsButton : MonoBehaviour {
+
+    //onGUI
+    void OnGUI()
+    {
+        Rect buttonRect = new Rect(10, 10, 150, 50);
+        string buttonText = Advertisement.IsReady() ? "Show Ad" : "Waiting...";
+
+        if (GUI.Button(buttonRect, buttonText))
+        {
+            Advertisement.Show();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
